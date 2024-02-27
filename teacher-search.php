0923c89c9929afd7ab264473604d50eb6182796a
irish060108 <160743330+irish060108@users.noreply.github.com>
2024-02-27 11:46:58 +0800

    Update teacher-search.php

--- teacher-search.php
+++ teacher-search.php
@@ -7,10 +7,10 @@ if (isset($_SESSION['admin_id']) &&
 		if (isset($_GET['searchKey'])) {
 
 		$search_key = $_GET['searchKey'];
-		include "../DB_connection.php";
-		include "data/teacher.php";
-		include "data/subject.php";
-		include "data/grade.php";
+		include "DB_connection.php";
+		include "teacherrr.php";
+		include "subjecttt.php";
+		include "gradeee.php";
 		$teachers = searchTeachers($search_key, $conn);
 ?>
 <!DOCTYPE html>
@@ -21,14 +21,14 @@ if (isset($_SESSION['admin_id']) &&
 	initial-scale=1.0">
 	<title>Admin - Search Teachers</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../css/style.css">
-	<link rel="icon" href="../logo.png">
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="logo.png">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<?php
-		include "inc/navbar.php";
+		include "navbar.php";
 		if ($teachers != 0){
 	?>
 	<div class="container mt-5">
@@ -143,15 +143,15 @@ if (isset($_SESSION['admin_id']) &&
 </html>
 <?php 
 }else {
-	header("Location: teacher.php");
+	header("Location: teachers.php");
 	exit;
 } 
 	}else {
-	header("Location: ../login.php");
+	header("Location: login.php");
 	exit;
 } 
 }else {
-	header("Location: ../login.php");
+	header("Location: login.php");
 	exit;
 } 
  ?>
