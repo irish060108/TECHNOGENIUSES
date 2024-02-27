5ac40a02e3918c0b62e6c2425261a8b46fae571a
irish060108 <160743330+irish060108@users.noreply.github.com>
2024-02-27 11:08:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,14 +15,14 @@ if (isset($_SESSION['admin_id']) &&
 	initial-scale=1.0">
 	<title>Admin - Home</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../css/style.css">
-	<link rel="icon" href="../logo.png">
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="logo.png">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<?php
-		include "inc/navbar.php";
+		include "navbar.php";
 	?>
 	<div class="container mt-5">
 		<div class="container text-center">
@@ -78,11 +78,11 @@ if (isset($_SESSION['admin_id']) &&
 </html>
 <?php 
 	}else {
-	header("Location: ../login.php");
+	header("Location: login.php");
 	exit;
 } 
 }else {
-	header("Location: ../login.php");
+	header("Location: login.php");
 	exit;
 } 
  ?>
