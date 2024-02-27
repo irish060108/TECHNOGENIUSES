cefc51f32dcc97275790dfca6ffa58889bc8c285
irish060108 <160743330+irish060108@users.noreply.github.com>
2024-02-27 11:44:57 +0800

    Update teacher-edit.php

--- teacher-edit.php
+++ teacher-edit.php
@@ -6,10 +6,10 @@ if (isset($_SESSION['admin_id']) &&
 
 	if ($_SESSION['role'] == 'Admin'){
 
-		include "../DB_connection.php";
-		include "data/teacher.php";
-		include "data/subject.php";
-		include "data/grade.php";
+		include "DB_connection.php";
+		include "teacherrr.php";
+		include "subjecttt.php";
+		include "gradeee.php";
 		$subjects = getAllSubjects($conn);
 		$grades = getAllGrades($conn);
 
@@ -29,14 +29,14 @@ if (isset($_SESSION['admin_id']) &&
 	initial-scale=1.0">
 	<title>Admin - Edit Teacher</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../css/style.css">
-	<link rel="icon" href="../logo.png">
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="logo.png">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<?php
-		include "inc/navbar.php";
+		include "navbar.php";
 		
 	?>
 	<div class="container mt-5">
@@ -44,7 +44,7 @@ if (isset($_SESSION['admin_id']) &&
 		   class="btn btn-dark">Go Back</a>
 		   <form method="post"
 		   		 class="shadow p-3 mt-5 form-w"
-			  	 action="req/teacher-edit.php">
+			  	 action="teacher-edittt.php">
 			<h3>Edit Teacher</h3><hr>
 			<?php if (isset($_GET['error'])) { ?>
 			<div class="alert alert-danger" role="alert">
@@ -206,7 +206,7 @@ if (isset($_SESSION['admin_id']) &&
 
 		<form method="post"
 		   		 class="shadow p-3 my-5 form-w"
-			  	 action="req/teacher-change.php"
+			  	 action="teacher-changeee.php"
 			  	 id="change_password">
 		<h3>Change Password</h3><hr>
 			<?php if (isset($_GET['perror'])) { ?>
